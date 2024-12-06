b7cd21d1b559d9bc723b6a119fde6a0204c02e8c
DieSonne-1 <ufimtseva.po@gmail.com>
2024-12-06 16:07:52 +0500

    Update settings.py

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'polina',
         'USER': 'postgres',
-        'PASSWORD': '1Parolorel!',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
